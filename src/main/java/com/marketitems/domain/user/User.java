840cbd67daa8facd731e02dad02172e4ece9d1ba
jauumgod <joaofs901@gmail.com>
2023-08-25 11:00:20 -0300

    feat: create user class

---
+++ src/main/java/com/marketitems/domain/user/User.java
@@ -0,0 +1,40 @@
+package com.marketitems.domain.user;
+
+
+import com.marketitems.dtos.UserDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name="users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+
+    public User(UserDTO data){
+        this.firstName = data.firstname();
+        this.lastName = data.lastname();
+        this.email = data.email();
+        this.password = data.password();
+
+    }
+}
---
+++ src/main/java/com/marketitems/dtos/ExceptionsDTO.java
@@ -0,0 +1,4 @@
+package com.marketitems.dtos;
+
+public record ExceptionsDTO(String message, String statusCode) {
+}
