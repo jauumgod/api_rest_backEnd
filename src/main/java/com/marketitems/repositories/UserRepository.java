160c41065f5282b4a4d75f1c6e9bb209c5e20205
jauumgod <joaofs901@gmail.com>
2023-08-25 10:59:33 -0300

    feat: create user repository

---
+++ src/main/java/com/marketitems/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.marketitems.repositories;
+
+import com.marketitems.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserById(Long id);
+}
