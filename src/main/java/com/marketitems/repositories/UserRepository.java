ef930a2479d7c24aad3e1def75d0c1cf7464793f
jauumgod <joaofs901@gmail.com>
2023-08-28 12:31:17 -0300

    feat: user repository add modify

--- src/main/java/com/marketitems/repositories/UserRepository.java
+++ src/main/java/com/marketitems/repositories/UserRepository.java
@@ -2,10 +2,9 @@ package com.marketitems.repositories;
 
 import com.marketitems.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserById(Long id);
 }
