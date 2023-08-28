26f91c89b53870e0131e47ebd9a9beceeccba11a
jauumgod <joaofs901@gmail.com>
2023-08-28 12:33:32 -0300

    feat: update item repository

--- src/main/java/com/marketitems/repositories/ItemListaRepository.java
+++ src/main/java/com/marketitems/repositories/ItemListaRepository.java
@@ -4,4 +4,5 @@ import com.marketitems.domain.listadecompras.ListaDeCompras;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemListaRepository extends JpaRepository<ListaDeCompras, Long> {
+    
 }
