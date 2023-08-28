cf4bd926d60823011dc201aaf6ee56c46c0df4a5
jauumgod <joaofs901@gmail.com>
2023-08-28 12:33:13 -0300

    feat: update class produto

--- src/main/java/com/marketitems/domain/items/Produto.java
+++ src/main/java/com/marketitems/domain/items/Produto.java
@@ -21,7 +21,6 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
-
     private String itemName;
     private BigDecimal price;
     private int quantity;
