f7cfcf93577469634550b6f1cb87f00dd821ad82
jauumgod <joaofs901@gmail.com>
2023-08-28 08:17:36 -0300

    feat: create itemlista repository

---
+++ src/main/java/com/marketitems/repositories/ItemListaRepository.java
@@ -0,0 +1,7 @@
+package com.marketitems.repositories;
+
+import com.marketitems.domain.listadecompras.ListaDeCompras;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemListaRepository extends JpaRepository<ListaDeCompras, Long> {
+}
