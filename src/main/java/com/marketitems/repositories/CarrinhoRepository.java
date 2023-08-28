2a2d0bd7f97b394b6533edbbc00b6f03383ad98c
jauumgod <joaofs901@gmail.com>
2023-08-28 12:32:57 -0300

    feat: update carrinho repository

--- src/main/java/com/marketitems/repositories/CarrinhoRepository.java
+++ src/main/java/com/marketitems/repositories/CarrinhoRepository.java
@@ -1,7 +1,11 @@
 package com.marketitems.repositories;
 
 import com.marketitems.domain.Cart.Carrinho;
+import com.marketitems.domain.items.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CarrinhoRepository extends JpaRepository<Carrinho, Long> {
+    Optional<Produto> findCarById(Long id);
 }
